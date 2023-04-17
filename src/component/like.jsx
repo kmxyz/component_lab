afed6b413c2663434775ff2d626a3aaa9f34591f
Mark <markxiao66@gmail.com>
2023-04-17 15:48:30 -0300

    add like component

---
+++ src/component/like.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+
+const Like = () => {
+    return (
+        <i
+            style={{ cursor: "pointer" }}
+            className="fa fa-heart-o"
+        ></i>
+    );
+}
+
+export default Like;
--- src/component/movies.js
+++ src/component/movies.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import {getMovies} from './fakeMovieService';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import Like from './like';
 
 class Movies extends Component {
     state = {
         items : getMovies(),
-        cal : ["Title", "Genre", "Stock", "Rate", "button"]
+        cal : ["Title", "Genre", "Stock", "Rate", "like", "button"]
     }
 
     deleteMovie = (movie) => {
@@ -15,25 +16,29 @@ class Movies extends Component {
     }
 
     render() { 
-        const { items: count } = this.state;
-        if (count.length === 0 ){return "there is no moive in the database."}
+        const { items } = this.state;
+        if (items.length === 0 ){return "there is no moive in the database."}
         return (
             <React.Fragment>
-            <p>Showing {count.length} moives in the database</p>
+            <p>Showing {items.length} moives in the database</p>
             <table className="table">
                 <thead>
                     <tr>
-                        {this.state.cal.map(cal => <th>{cal}</th>)}
+                        {this.state.cal.map(cal => <th key={cal}>{cal}</th>)}
                     </tr>
                 </thead>
                 <tbody>
                     {this.state.items.map(item => (
-                    <tr>
+                    <tr key={item._id}>
                         <td>{item.title}</td>
                         <td>{item.genre.name}</td>
                         <td>{item.numberInStock}</td>
                         <td>{item.dailyRentalRate}</td>
-                        <td><button onClick={() => this.deleteMovie(item)} className='button.btn.btn-danger.btn-sm'>Delete</button></td>
+                        <td><Like /></td>
+                        <td><button 
+                            onClick={() => this.deleteMovie(item)} 
+                            className='btn btn-danger btn-sm'>Delete</button>
+                        </td>
                     </tr>
                     ))}
                 </tbody>
