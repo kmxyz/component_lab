2e109366465068250fea906521205f3017bb71e9
Mark <markxiao66@gmail.com>
2023-04-17 16:19:41 -0300

    fixed the bug of like component

--- src/component/like.jsx
+++ src/component/like.jsx
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import 'font-awesome/css/font-awesome.min.css';
+import React, { Component } from "react";
+import "font-awesome/css/font-awesome.min.css";
 
-const Like = () => {
-    return (
-        <i
-            style={{ cursor: "pointer" }}
-            className="fa fa-heart-o"
-        ></i>
-    );
-}
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (props.like !== true) {
+    classes += "-o";
+  }
+  return (
+    <i
+      style={{ cursor: "pointer" }}
+      className={classes}
+      onClick={props.onClicked}
+    ></i>
+  );
+};
 
 export default Like;
---
+++ src/component/movies.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { getMovies } from "./fakeMovieService";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import Like from "./like";
+
+class Movies extends Component {
+  state = {
+    items: getMovies(),
+    cal: ["Title", "Genre", "Stock", "Rate", "like", "button"],
+  };
+
+  deleteMovie = (movie) => {
+    const movies = this.state.items.filter((m) => m._id !== movie._id);
+    this.setState({ items: movies });
+  };
+
+  headleOnClick = (movie) => {
+    const movies = [...this.state.items];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movie };
+    movies[index].like = !movies[index].like;
+    this.setState({ items: movies });
+  };
+
+  render() {
+    const { items } = this.state;
+    if (items.length === 0) {
+      return "there is no moive in the database.";
+    }
+    return (
+      <React.Fragment>
+        <p>Showing {items.length} moives in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              {this.state.cal.map((cal) => (
+                <th key={cal}>{cal}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.items.map((item) => (
+              <tr key={item._id}>
+                <td>{item.title}</td>
+                <td>{item.genre.name}</td>
+                <td>{item.numberInStock}</td>
+                <td>{item.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    like={item.like}
+                    onClicked={() => this.headleOnClick(item)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.deleteMovie(item)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
